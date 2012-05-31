69157c26f3caa91cb482905f26f8aecbdc8f86b8
Tyler Boyd <tboyd47@gmail.com>
2012-05-31 13:00:07 -0400

    cleanup;jquery;table layout

---
+++ app/assets/javascripts/entries.js
@@ -0,0 +1,13 @@
+$(function() {
+	$("#addButton").click(function() {
+		var id = new Date().getTime();
+		$("tbody").last().append("<tr><td><input name='user[entries_attributes]["+id+"][body]'></input></td><td><input name='user[entries_attributes]["+id+"][priority]'></input></td><td><input name='user[entries_attributes]["+id+"][due_date]'></input></td><td><button type='button' class='delButton' data-id='"+id+"'>Delete</button></td></tr>");
+	});
+
+	$(".delButton").live("click", function() {
+		var row = $(this).closest("tr");
+		row.hide();
+		var newInputName = row.find("input").first().attr("name").replace("body","_destroy");
+		$(this).after("<input type='hidden' name='"+newInputName+"' value='1'></input>");
+	});
+});
--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,22 +1,13 @@
 class EntriesController < ApplicationController
   def index
-    setup_index
   end
 
   def edit
-    current_user.entries.push Entry.new
     render "edit"
   end
 
   def update
     current_user.update_attributes(params[:user])
-    setup_index
     render "index"
-  end
-
-  private
-
-  def setup_index
-    @entries = current_user.entries
   end
 end
