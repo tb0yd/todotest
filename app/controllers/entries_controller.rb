ef98082e1dd82ea2daa9bf30a9f93f963e126920
Tyler Boyd <tboyd47@gmail.com>
2012-06-04 15:49:34 -0400

    only render with layout if html requested

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -2,20 +2,28 @@ class EntriesController < ApplicationController
   before_filter :prepare_vars
 
   def index
+    conditional_render :action => :index
   end
 
   def edit
-    render "edit"
+    conditional_render :action => :edit
   end
 
   def update
     current_user.update_attributes(params[:user])
-    render "index"
+    conditional_render :action => :index
   end
 
   private
 
   def prepare_vars
     @entries = current_user.entries.order(params[:order])
+  end
+
+  def conditional_render(opts)
+    respond_to do |format|
+      format.html { render opts }
+      format.js { render opts.merge(:layout => false) }
+    end
   end
 end
